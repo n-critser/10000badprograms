3a15fae50486f14f87224a563793a1bef9fdb4ec
n-critser <none@none>
2019-05-16 23:27:37 -0400

    start a congruence app in c++

--- cpp/congruence/app/main.cpp
+++ cpp/congruence/app/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <lib.hpp>
 
+bool is_congruent(int left , int right, int mod);
+
 int main()
 {
+  int congruent = is_congruent(2,3,5);
     dummy();
+    std::cout << "congruent:" << congruent << std::endl;
     return 0;
+}
+
+bool is_congruent(int left , int right, int mod){
+
+  int cong = (left - right) % mod ;
+  std::cout << "cong:" << cong << std::endl;
+  bool res=false;
+  if ( cong == 0){
+    res = true;
+  }
+  
+  return res;
 }
