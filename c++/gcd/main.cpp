62bcc3e4e25e46b73449b3c480b6a372387f1fa0
nick <2449874+n-critser@users.noreply.github.com>
2019-05-29 09:20:28 -0400

    gcd in cpp

--- c++/gcd/main.cpp
+++ c++/gcd/main.cpp
@@ -7,9 +7,32 @@ unsigned gcd(int left, int right);
 int main(int arc,char**argv){
 
   std::cout << "hello world" << '\n';
+
+  for (int i = 6; i> 0; i--){
+    std::cout << "gcd :(32 ,"<<  i << ") ="<< gcd(32,i) << '\n';
+  }
   return 0;
 }
 
 unsigned gcd(int left, int right){
-  return 0;
+
+  int r=-1;
+  int max = left > right ? left: right;
+  int min = left <= right? left: right;
+  int gcd = 0;
+  do {
+    // std::cout<< "max:"<<max <<","<<"min:"<<min<< '\n';
+    if ( (r = (max % min)) == 0){
+
+      gcd = min;
+    } else {
+      int minus= max - min;
+      max = minus > min ? minus: min;
+      min = minus <= min? minus: min;
+
+    }
+    
+  } while (r !=0);
+  
+  return gcd;
 }
